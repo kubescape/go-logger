55f46b8ba11297897f2975b80bbfce9c6dff019a
Amir Malka <amirm@armosec.io>
2023-03-13 09:11:41 +0200

    use utf replacement string
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- helpers/datastructures.go
+++ helpers/datastructures.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+const InvalidUtf8ReplacementString = "\uFFFD"
+
 type StringObj struct {
 	key   string
 	value string
@@ -25,9 +27,11 @@ type InterfaceObj struct {
 	value interface{}
 }
 
-func Error(e error) *ErrorObj                         { return &ErrorObj{key: "error", value: e} }
-func Int(k string, v int) *IntObj                     { return &IntObj{key: k, value: v} }
-func String(k, v string) *StringObj                   { return &StringObj{key: k, value: strings.ToValidUTF8(v, "")} }
+func Error(e error) *ErrorObj     { return &ErrorObj{key: "error", value: e} }
+func Int(k string, v int) *IntObj { return &IntObj{key: k, value: v} }
+func String(k, v string) *StringObj {
+	return &StringObj{key: k, value: strings.ToValidUTF8(v, InvalidUtf8ReplacementString)}
+}
 func Interface(k string, v interface{}) *InterfaceObj { return &InterfaceObj{key: k, value: v} }
 func Time() *StringObj {
 	return &StringObj{key: "time", value: time.Now().Format("2006-01-02 15:04:05")}
